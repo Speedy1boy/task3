022b7c15ef05799b5ef3304ef691b081706b7625
Vladislav <vladkrw@gmail.com>
2026-03-12 17:44:13 +0500

    task 3

---
+++ src/main/java/ru/vladislav/JavaNaumen/Config.java
@@ -0,0 +1,54 @@
+package ru.vladislav.JavaNaumen;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import ru.vladislav.JavaNaumen.entity.Movie;
+
+@Configuration
+public class Config {
+    @Value("${app.name}")
+    private String name;
+
+    @Value("${app.version}")
+    private String version;
+
+    @Bean
+    public CommandLineRunner commandLineRunner(CommandProcessor commandProcessor) {
+        return args -> {
+            try (var scanner = new Scanner(System.in)) {
+                System.out.println("Введите команду \"exit\" для выхода.");
+                while (true) {
+                    System.out.print("> ");
+                    var input = scanner.nextLine();
+                    if ("exit".equalsIgnoreCase(input.trim())) {
+                        System.out.println("Выход из программы...");
+                        break;
+                    }
+                    commandProcessor.processCommand(input);
+                }
+            }
+        };
+    }
+
+    @Bean
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    public List<Movie> movieContainer() {
+        return new ArrayList<>();
+    }
+
+    @PostConstruct
+    public void printInfo() {
+        System.out.printf("%s v%s.%n", name, version);
+    }
+}
---
+++ src/main/java/ru/vladislav/JavaNaumen/entity/Movie.java
@@ -0,0 +1,36 @@
+package ru.vladislav.JavaNaumen.entity;
+
+public class Movie {
+    private Long id;
+    private String name;
+    private String genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Movie(%d, %s, %s)", id, name, genre);
+    }
+}
